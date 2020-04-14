b1dae19d1106b7f19f3ca1e49eba500fe624c611
kelvin algusto <kelvin.zuzor@gmail.com>
2020-04-14 17:13:06 -0300

    finalizando fornecedor

--- js/estoque.js
+++ js/estoque.js
@@ -136,7 +136,7 @@ $.ajax({
 
     });
     objFornecedor = response;
-    // console.log(objFornecedor)
+    console.log(objFornecedor)
     //popula a tabela
     $.each(response, function (i, item) {
 
@@ -147,7 +147,8 @@ $.ajax({
 
             '<td>' + item.Celular + '</td>' +
             '<td>' + item.Telefone + '</td>' +
-            '<td>' + item.Email_Representante + '</td>' +
+            // '<td>' + item.Email_Representante + '</td>' +
+            '<td>' + item.Estatus_Fornecedor + '</td>' +
             '<td>' +
             '<button type="button" onclick="editarFornecedor(' + item.idFornecedor +
             ')"  class="btn btn-info " >' +
@@ -155,16 +156,17 @@ $.ajax({
             '<i class="fas fa-pencil-alt"></i> ' +
             '</button>' +
             '</td>' +
-/*
-            '<td><button type="button" onclick="excluir(' + item.idProduto + ',' + 3 +
-            ')"  class="btn btn-danger" data-toggle="modal" data-target="#excluirModal">' +
-            '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M68.37,6.88c-0.63156,0.08063 -1.22281,0.34938 -1.72,0.7525l-24.1875,19.8875h-14.9425c-0.1075,0 -0.215,0 -0.3225,0c-0.1075,0 -0.215,0 -0.3225,0c-1.63937,0.30906 -2.82187,1.76031 -2.795,3.44v13.76c0,1.89469 1.54531,3.44 3.44,3.44h0.645l10.105,54.18c-0.06719,0.71219 0.08063,1.42438 0.43,2.0425l4.6225,24.725c-0.02687,0.55094 0.08063,1.11531 0.3225,1.6125l2.4725,13.33c0,0.04031 0,0.06719 0,0.1075c1.69313,8.00875 8.65375,14.0825 16.8775,14.0825h46.01c8.19688,0 15.53375,-5.97969 16.8775,-14.19c0,-0.04031 0,-0.06719 0,-0.1075l2.4725,-13.2225c0,-0.04031 0,-0.06719 0,-0.1075l0.1075,-0.215c0.16125,-0.44344 0.24188,-0.92719 0.215,-1.3975l4.6225,-24.51c0.40313,-0.68531 0.55094,-1.47812 0.43,-2.2575l10.105,-54.0725h0.645c1.89469,0 3.44,-1.54531 3.44,-3.44v-13.76c0,-1.89469 -1.54531,-3.44 -3.44,-3.44h-10.32l-22.4675,-16.0175c-0.61812,-0.47031 -1.38406,-0.65844 -2.15,-0.5375c-0.18812,0.05375 -0.36281,0.12094 -0.5375,0.215l-22.0375,9.46l-16.0175,-13.0075c-0.72562,-0.59125 -1.65281,-0.86 -2.58,-0.7525zM68.8,14.7275l15.8025,12.7925h-31.2825zM109.7575,16.985l14.835,10.535h-29.1325l-3.5475,-2.9025zM30.96,34.4h110.08v6.88h-30.2075c-0.34937,-0.09406 -0.71219,-0.13437 -1.075,-0.1075c-0.1075,0.02688 -0.215,0.06719 -0.3225,0.1075h-36.8725c-0.25531,-0.02687 -0.49719,-0.02687 -0.7525,0h-40.85zM42.785,48.16h10.75l-5.375,5.375zM63.425,48.16h7.31l10.32,10.32l-13.975,13.975l-13.975,-13.975zM80.625,48.16h10.75l-5.375,5.375zM101.265,48.16h7.31l10.32,10.32l-13.975,13.975l-13.975,-13.975zM118.465,48.16h10.75l-5.375,5.375zM35.5825,50.8475l7.6325,7.6325l-5.16,5.2675zM136.4175,50.8475l-2.365,13.0075l-5.2675,-5.375zM48.16,63.3175l14.0825,14.0825l-14.0825,14.0825l-5.4825,-5.4825c-0.16125,-0.16125 -0.34937,-0.30906 -0.5375,-0.43l-2.58,-13.6525zM86,63.425l14.0825,13.975l-14.0825,14.0825l-13.975,-14.0825zM123.84,63.425l8.6,8.4925l-2.4725,13.545c-0.24187,0.14781 -0.45687,0.33594 -0.645,0.5375l-5.4825,5.4825l-14.0825,-14.0825zM67.08,82.2375l14.0825,14.0825l-14.0825,14.0825l-14.0825,-14.0825zM104.92,82.2375l14.0825,14.0825l-14.0825,14.0825l-14.0825,-14.0825zM48.16,101.1575l14.0825,14.0825l-12.255,12.255l-4.4075,-23.7575zM86,101.1575l14.0825,14.0825l-14.0825,14.0825l-14.0825,-14.0825zM123.84,101.1575l2.58,2.58l-4.4075,23.7575l-12.255,-12.255zM67.08,120.0775l14.0825,14.0825l-12.3625,12.3625l-14.0825,-14.0825zM104.92,120.0775l12.3625,12.3625l-14.0825,14.0825l-12.3625,-12.3625zM86,138.9975l12.3625,12.3625h-24.725zM52.3525,139.75l11.61,11.61h-0.9675c-4.85094,0 -9.04344,-3.53406 -10.105,-8.6zM119.6475,139.75l-0.5375,3.01c0,0.04031 0,0.06719 0,0.1075c-0.76594,4.81063 -5.25406,8.4925 -10.105,8.4925h-0.9675z"></path></g></g></svg>' +
-            '</button></td>' +
-*/
+            /*
+                        '<td><button type="button" onclick="excluir(' + item.idProduto + ',' + 3 +
+                        ')"  class="btn btn-danger" data-toggle="modal" data-target="#excluirModal">' +
+                        '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M68.37,6.88c-0.63156,0.08063 -1.22281,0.34938 -1.72,0.7525l-24.1875,19.8875h-14.9425c-0.1075,0 -0.215,0 -0.3225,0c-0.1075,0 -0.215,0 -0.3225,0c-1.63937,0.30906 -2.82187,1.76031 -2.795,3.44v13.76c0,1.89469 1.54531,3.44 3.44,3.44h0.645l10.105,54.18c-0.06719,0.71219 0.08063,1.42438 0.43,2.0425l4.6225,24.725c-0.02687,0.55094 0.08063,1.11531 0.3225,1.6125l2.4725,13.33c0,0.04031 0,0.06719 0,0.1075c1.69313,8.00875 8.65375,14.0825 16.8775,14.0825h46.01c8.19688,0 15.53375,-5.97969 16.8775,-14.19c0,-0.04031 0,-0.06719 0,-0.1075l2.4725,-13.2225c0,-0.04031 0,-0.06719 0,-0.1075l0.1075,-0.215c0.16125,-0.44344 0.24188,-0.92719 0.215,-1.3975l4.6225,-24.51c0.40313,-0.68531 0.55094,-1.47812 0.43,-2.2575l10.105,-54.0725h0.645c1.89469,0 3.44,-1.54531 3.44,-3.44v-13.76c0,-1.89469 -1.54531,-3.44 -3.44,-3.44h-10.32l-22.4675,-16.0175c-0.61812,-0.47031 -1.38406,-0.65844 -2.15,-0.5375c-0.18812,0.05375 -0.36281,0.12094 -0.5375,0.215l-22.0375,9.46l-16.0175,-13.0075c-0.72562,-0.59125 -1.65281,-0.86 -2.58,-0.7525zM68.8,14.7275l15.8025,12.7925h-31.2825zM109.7575,16.985l14.835,10.535h-29.1325l-3.5475,-2.9025zM30.96,34.4h110.08v6.88h-30.2075c-0.34937,-0.09406 -0.71219,-0.13437 -1.075,-0.1075c-0.1075,0.02688 -0.215,0.06719 -0.3225,0.1075h-36.8725c-0.25531,-0.02687 -0.49719,-0.02687 -0.7525,0h-40.85zM42.785,48.16h10.75l-5.375,5.375zM63.425,48.16h7.31l10.32,10.32l-13.975,13.975l-13.975,-13.975zM80.625,48.16h10.75l-5.375,5.375zM101.265,48.16h7.31l10.32,10.32l-13.975,13.975l-13.975,-13.975zM118.465,48.16h10.75l-5.375,5.375zM35.5825,50.8475l7.6325,7.6325l-5.16,5.2675zM136.4175,50.8475l-2.365,13.0075l-5.2675,-5.375zM48.16,63.3175l14.0825,14.0825l-14.0825,14.0825l-5.4825,-5.4825c-0.16125,-0.16125 -0.34937,-0.30906 -0.5375,-0.43l-2.58,-13.6525zM86,63.425l14.0825,13.975l-14.0825,14.0825l-13.975,-14.0825zM123.84,63.425l8.6,8.4925l-2.4725,13.545c-0.24187,0.14781 -0.45687,0.33594 -0.645,0.5375l-5.4825,5.4825l-14.0825,-14.0825zM67.08,82.2375l14.0825,14.0825l-14.0825,14.0825l-14.0825,-14.0825zM104.92,82.2375l14.0825,14.0825l-14.0825,14.0825l-14.0825,-14.0825zM48.16,101.1575l14.0825,14.0825l-12.255,12.255l-4.4075,-23.7575zM86,101.1575l14.0825,14.0825l-14.0825,14.0825l-14.0825,-14.0825zM123.84,101.1575l2.58,2.58l-4.4075,23.7575l-12.255,-12.255zM67.08,120.0775l14.0825,14.0825l-12.3625,12.3625l-14.0825,-14.0825zM104.92,120.0775l12.3625,12.3625l-14.0825,14.0825l-12.3625,-12.3625zM86,138.9975l12.3625,12.3625h-24.725zM52.3525,139.75l11.61,11.61h-0.9675c-4.85094,0 -9.04344,-3.53406 -10.105,-8.6zM119.6475,139.75l-0.5375,3.01c0,0.04031 0,0.06719 0,0.1075c-0.76594,4.81063 -5.25406,8.4925 -10.105,8.4925h-0.9675z"></path></g></g></svg>' +
+                        '</button></td>' +
+            */
             '</tr>';
 
     });
     $('#tabelaFornecedor').append(tblFornecedor);
+    esconderInativos("tabelaFornecedor", "hide", 5);
 });
 
 /* funçao traz combo*/
@@ -592,7 +594,7 @@ function atualizarCategoria(_id) {
 
 $("#fornecedor_modal").click(() => {
     $("#botaoEnviarFornecedor").html('');
-    $("#checkboxFornecedor").hide();//esconde botao excluir
+    $("#checkboxFornecedor").hide(); //esconde botao excluir
 
     let botao = '<button type="button" class="btn btn-success ml-4 Font-weight-bold"' +
         ' onclick="enviarFornecedor()"  name="button">  ' +
@@ -647,7 +649,7 @@ function editarFornecedor(_id) {
     let data;
     idFornecedor = _id;
     $("#botaoEnviarFornecedor").html(''); //limpa div
-    $("#checkboxFornecedor").show();//esconde botao excluir
+    $("#checkboxFornecedor").show(); //esconde botao excluir
     //seleciona no objteto o elemento da linha 
     $.each(objFornecedor, function (i, item) {
         if (item.idFornecedor == _id) {
@@ -681,6 +683,7 @@ function atualizarFornecedor(_id) {
         celular: "",
         email: "",
         representante: "",
+        EstatusFornecedor: "",
         idUsuario: 0
     }
 
@@ -690,6 +693,7 @@ function atualizarFornecedor(_id) {
     objFornecedor.telefone = $("#telefone").val();
     objFornecedor.email = $("#email").val();
     objFornecedor.fantasia = $("#fantasia").val();
+    objFornecedor.EstatusFornecedor = $("#statusFornecedor").val();
     objFornecedor.idUsuario = user.idUsuario;
 
     //let put_url_fornecedor = 'http://localhost:3000/fornecedores/atualizar/' + _id;
@@ -700,7 +704,18 @@ function atualizarFornecedor(_id) {
         type: 'PUT',
         data: objFornecedor
     }).done(function (response) {
-
-        document.location.reload();
+      
+      document.location.reload();
     });
 };
+
+$("#inativosFornecedores").click(() => {
+    let checkbox_fornecedor = $("input[name='inativosFornecedores']:checked").val();
+
+    if (checkbox_fornecedor == true) {
+        esconderInativos("tabelaFornecedor", "show", 5);
+    } else {
+        esconderInativos("tabelaFornecedor", "hide", 5);
+    }
+
+})
