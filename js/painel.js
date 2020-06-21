ff11fe76fa307a42a032bb3c79e3339c4297e4cd
kelvin algusto <kelvin.zuzor@gmail.com>
2020-06-21 20:00:03 -0300

    Update painel.js

--- js/painel.js
+++ js/painel.js
@@ -193,7 +193,7 @@ $.ajax({
     vetorDados.push(parseFloat(d.Lucro.toFixed(2)));
     vetorBarra.push(d.Categoria);
   });
-  somarTotalLucro= somarTotalLucro + 127,5
+  somarTotalLucro= somarTotalLucro + 127
   $('#painel_estoque_lucro').text(somarTotalLucro.toFixed(2) );
 
   graficoLinhas(vetorDados, vetorBarra, "widgetChart8", 'Produtos');
